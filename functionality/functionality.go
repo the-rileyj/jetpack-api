9996edd3ab2afaef27dbcc0f7d815620bf35a6dc
Riley Johnson <rileyjohnson99@hotmail.com>
2019-06-04 22:04:59 -0400

    Potentially fixed github hook problem

--- functionality/functionality.go
+++ functionality/functionality.go
@@ -4,8 +4,9 @@ import (
 	"bufio"
 	"crypto/hmac"
 	"crypto/sha1"
-	"encoding/base64"
+	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -30,6 +31,64 @@ type JetpackArticles struct {
 	Articles        []JetpackArticle `json:"articles"`
 	MainTitle       string           `json:"mainTitle"`
 	MainDescription string           `json:"mainDescription"`
+}
+
+func signBody(secret, body []byte) []byte {
+	computed := hmac.New(sha1.New, secret)
+	computed.Write(body)
+	return []byte(computed.Sum(nil))
+}
+
+func verifySignature(secret []byte, signature string, body []byte) bool {
+
+	const signaturePrefix = "sha1="
+	const signatureLength = 45 // len(SignaturePrefix) + len(hex(sha1))
+
+	if len(signature) != signatureLength || !strings.HasPrefix(signature, signaturePrefix) {
+		return false
+	}
+
+	actual := make([]byte, 20)
+	hex.Decode(actual, []byte(signature[5:]))
+
+	return hmac.Equal(signBody(secret, body), actual)
+}
+
+type HookContext struct {
+	Signature string
+	Event     string
+	Id        string
+	Payload   []byte
+}
+
+func ParseHook(secret []byte, req *http.Request) (*HookContext, error) {
+	hc := HookContext{}
+
+	if hc.Signature = req.Header.Get("x-hub-signature"); len(hc.Signature) == 0 {
+		return nil, errors.New("No signature!")
+	}
+
+	if hc.Event = req.Header.Get("x-github-event"); len(hc.Event) == 0 {
+		return nil, errors.New("No event!")
+	}
+
+	if hc.Id = req.Header.Get("x-github-delivery"); len(hc.Id) == 0 {
+		return nil, errors.New("No event Id!")
+	}
+
+	body, err := ioutil.ReadAll(req.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !verifySignature(secret, hc.Signature, body) {
+		return nil, errors.New("Invalid signature")
+	}
+
+	hc.Payload = body
+
+	return &hc, nil
 }
 
 func parseToArticles(r io.Reader) (JetpackArticles, error) {
@@ -133,27 +192,13 @@ func GetHandlerForGetJetpackArticles(getJetpackArticles func() *JetpackArticles)
 
 func GetHandlerForUpdateJetpackArticlesHandler(secret string, updateJetpackArticles func() error) func(c *gin.Context) {
 	return func(c *gin.Context) {
-		requestHMAC := hmac.New(sha1.New, []byte(secret))
+		// requestHMAC := hmac.New(sha1.New, []byte(secret))
 
-		bodyBytes, err := ioutil.ReadAll(c.Request.Body)
+		// bodyBytes, err := ioutil.ReadAll(c.Request.Body)
 
-		if err != nil {
-			c.Status(500)
-			c.Writer.Write([]byte(fmt.Sprintf("Articles Update Failed: %s", err.Error())))
+		hookContext, err := ParseHook([]byte(secret), c.Request)
 
-			return
-		}
-
-		requestHMAC.Write(bodyBytes)
-
-		if c.GetHeader("X-Hub-Signature") != base64.StdEncoding.EncodeToString(requestHMAC.Sum(nil)) {
-			c.Status(500)
-			c.Writer.Write([]byte(fmt.Sprintf("Articles Update Failed: Signature sent and signature generated do not match")))
-
-			return
-		}
-
-		err = updateJetpackArticles()
+		fmt.Println("Received %s", hookContext.Event)
 
 		if err != nil {
 			c.Status(500)
@@ -161,6 +206,24 @@ func GetHandlerForUpdateJetpackArticlesHandler(secret string, updateJetpackArtic
 
 			return
 		}
+
+		// requestHMAC.Write(bodyBytes)
+
+		// if c.GetHeader("X-Hub-Signature") != base64.StdEncoding.EncodeToString(requestHMAC.Sum(nil)) {
+		// 	c.Status(500)
+		// 	c.Writer.Write([]byte(fmt.Sprintf("Articles Update Failed: Signature sent and signature generated do not match")))
+
+		// 	return
+		// }
+
+		// err = updateJetpackArticles()
+
+		// if err != nil {
+		// 	c.Status(500)
+		// 	c.Writer.Write([]byte(fmt.Sprintf("Articles Update Failed: %s", err.Error())))
+
+		// 	return
+		// }
 
 		c.Status(202)
 		c.Writer.Write([]byte("Articles Updated Successfully"))
